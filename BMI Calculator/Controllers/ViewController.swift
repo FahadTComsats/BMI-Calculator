84085b2be5adbb073db1be7dade597ebfe8b732c
Fahad Tariq <47610084+FahadTComsats@users.noreply.github.com>
2020-06-12 03:24:34 +0500

    Working on 2nd View Controller

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    
+    @IBAction func heightValueChanged(_ sender: UISlider) {
+        
+       // print(String(format:"%.2f", sender.value))
+        let height = String(format:"%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+        
+    }
+    
+    @IBAction func weightValueChanged(_ sender: UISlider) {
+        
+        //print(String(format:"%.0f", sender.value))
+        let weight = String(format:"%.0f", sender.value)
+        weightLabel.text = "\(weight)kg"
+         
+        
+    }
+    
+    
+    @IBAction func calculateBMI(_ sender: UIButton) {
+        
+        let weight = weightSlider.value
+        let height = heightSlider.value
+        
+        let bmi = weight/pow(height, 2)
+        print(bmi)
+        
+    }
+    
+    
 }
 
